6997651b5657f4466b5cebbe1a8d761869c91a53
jimin <holiday31@naver.com>
2021-02-19 12:16:40 +0900

    spring boot

---
+++ TestPR/src/test/java/com/bugcatcher/TestPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugcatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
